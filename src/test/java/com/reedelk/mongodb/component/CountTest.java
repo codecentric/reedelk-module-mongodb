d378f8e68e703377928642ab42cef0ec02d1cad7
lorenzo <lorenzo@lorenzo.info.com>
2020-04-15 21:09:31 +0200

    code

--- src/test/java/com/reedelk/mongodb/component/CountTest.java
+++ src/test/java/com/reedelk/mongodb/component/CountTest.java
@@ -1,8 +1,24 @@
 package com.reedelk.mongodb.component;
 
 import com.reedelk.mongodb.internal.ClientFactory;
+import com.reedelk.mongodb.internal.exception.MongoDBCountException;
+import com.reedelk.mongodb.internal.exception.MongoDBDeleteException;
+import com.reedelk.runtime.api.commons.ModuleContext;
+import com.reedelk.runtime.api.message.Message;
+import com.reedelk.runtime.api.message.MessageAttributes;
+import com.reedelk.runtime.api.message.MessageBuilder;
+import com.reedelk.runtime.api.script.dynamicvalue.DynamicObject;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doAnswer;
 
 class CountTest extends AbstractMongoDBTest {
 
@@ -25,4 +41,65 @@ class CountTest extends AbstractMongoDBTest {
         }
     }
 
+    @Test
+    void shouldCorrectlyCountAllDocumentsWithoutFilter() {
+        // Given
+        insertDocument("{name:'Olav', surname: 'Zipser', age: 55}");
+        insertDocument("{name:'Mark', surname: 'Anton', age: 32}");
+
+        component.initialize();
+
+        Message input = MessageBuilder.get().empty().build();
+
+        // When
+        Message actual = component.apply(context, input);
+
+        // Then
+        Long count = actual.payload();
+        assertThat(count).isEqualTo(2L);
+    }
+
+    @Test
+    void shouldCorrectlyCountAllDocumentsWithFilter() {
+        // Given
+        insertDocument("{name:'Olav', surname: 'Zipser', age: 55}");
+        insertDocument("{name:'Mark', surname: 'Anton', age: 32}");
+        insertDocument("{name:'Josh', surname: 'Red', age: 49}");
+
+        component.setFilter(DynamicObject.from("{ name: /.*a.*/ }"));
+        component.initialize();
+
+        Message input = MessageBuilder.get().empty().build();
+
+        // When
+        Message actual = component.apply(context, input);
+
+        // Then
+        Long count = actual.payload();
+        assertThat(count).isEqualTo(2L);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenFilterEvaluatesToNull() {
+        // Given
+        DynamicObject filter = DynamicObject.from("#[context.myFilter]", new ModuleContext(10L));
+        component.setFilter(filter);
+        component.initialize();
+
+        Message input = MessageBuilder.get().empty().build();
+
+        doAnswer(invocation -> Optional.empty())
+                .when(scriptService)
+                .evaluate(filter, context, input);
+
+        // When
+        MongoDBCountException thrown =
+                assertThrows(MongoDBCountException.class, () -> component.apply(context, input));
+
+        // Then
+        assertThat(thrown)
+                .hasMessage("The Count filter was null. " +
+                        "I cannot execute Count operation with a null filter " +
+                        "(DynamicValue=[#[context.myFilter]]).");
+    }
 }
