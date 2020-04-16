9e54ec8c5c930566cab20e64e8572a4cb716ad35
lorenzo <lorenzo@lorenzo.info.com>
2020-04-16 10:04:49 +0200

    code

--- src/main/resources/scripts/test.js
+++
@@ -1,6 +0,0 @@
-function test(context, message) {
-    var stringified = JSON.stringify(message.payload());
-    Log.info(message.payload());
-	var text = 'test function';
-	return text + ' result';
-}
